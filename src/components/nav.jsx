b4e362745877ddb38f760475936b64f3d02f055e
krendus <xaiyax01@gmail.com>
2023-12-25 11:10:32 +0100

    fix

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -26,7 +26,7 @@ const Nav = () => {
       </h1>
       <div className='flex items-center'>
         <div  className='flex items-center gap-x-2 pr-4 border-r border-[#DEE0E6]'>
-          <button className='bg-primary rounded-md  px-3 md:px-6 py-2 text-sm text-white'>Upgrade</button>
+          <button className='bg-primary rounded-md hidden sm:flex px-3 md:px-6 py-2 text-sm text-white'>Upgrade</button>
           <div className='flex gap-x-5 px-2 md:px-5 py-2 bg-[#F3F3F3] rounded-sm text-sm text-primary'>
             <span className='font-bold'>55</span>
             <span className='hidden md:block'>Credits Remaining</span>
@@ -37,7 +37,7 @@ const Nav = () => {
             <button className='text-light-gray hover:text-primary p-2'>
               <BellIcon />
             </button>
-            <div className='hidden group-hover:flex flex-col gap-y-5 min-h-[300px] max-h-[300px] absolute right-0 top-[40px] w-[400px] p-5 rounded-lg bg-white shadow-modal'>
+            <div className='hidden group-hover:flex flex-col gap-y-5 min-h-[300px] max-h-[300px] absolute -right-14 md:right-0 top-[40px] w-[300px] md:w-[400px] p-5 rounded-lg bg-white shadow-modal z-50'>
               <ActivityCard 
                 content={"Your recent notification is completed."}
                 date={"Sep 04th, 2023"}
